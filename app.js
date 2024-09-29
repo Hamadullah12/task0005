0ad4f161a6d7d13f2e02d231ac7a1cafa3b38850
Hamadullah12 <hamadfms05@gmail.com>
2024-09-29 10:41:24 +0500

    having some doubts at this place

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 const ageCalculate = () => {
-    
+    const today = new Date();
+    const inputDate = new Date(document.getElementById("date-input").value);
+
+    const birthDetails = {
+        date: inputDate.getDate(),
+        month: inputDate.getMonth() + 1,
+        year: inputDate.getFullYear(),
+    };
+    const currentYear = today.getFullYear();
+    const currentMonth = today.getMonth() + 1;
+    const currentDate = today.getDate();
+
+
+    if (isFututreData(birthDetails, currentYear, currentMonth, currentDate)) {
+        alert()
+    }
+
 }
+document.getElementById("calc-age-btn").
+    addEventListener("click", ageCalculate);
